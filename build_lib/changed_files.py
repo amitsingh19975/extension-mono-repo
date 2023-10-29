01d7e3caba1309b6174995fe7c4c6d35ee58318e
Amit Singh <amitsingh19975@gmail.com>
2023-10-30 00:52:16 +0530

    parse path properly

--- build_lib/changed_files.py
+++ build_lib/changed_files.py
@@ -4,6 +4,35 @@ from typing import List, MutableSet, Set, cast
 from .parse_module_dep import DependencyFiles, Module
 from .diagnostics import DiagnosticBase, DiagnosticKind, DiagnosticLocation
 
+def parse_space_separated_paths_escape(line: str, paths: List[Path], resolved_base_path: Path) -> None:
+    path = ''
+    pieces: List[str] = []
+    escape = False
+    for c in line:
+        if escape:
+            path += c
+            escape = False
+        elif c == '\\':
+            escape = True
+        elif c == ' ':
+            pieces.append(path)
+            path = ''
+        else:
+            path += c
+    
+    if path != '':
+        pieces.append(path)
+    
+    for piece in pieces:
+        path_buf = Path(piece.strip())
+        if not path_buf.is_absolute():
+            path_buf = resolved_base_path / piece
+        
+        if path_buf.exists() and path_buf.is_file():
+            paths.append(path_buf)
+
+
+
 def parse_changed_files(diagnostic: DiagnosticBase, base_path: Path = Path(".")) -> List[Path]:
     file_path = base_path / 'changed_files.txt'
     location = DiagnosticLocation(path=file_path, prefix=None, resolved_base_path=base_path.resolve())
@@ -21,13 +50,9 @@ def parse_changed_files(diagnostic: DiagnosticBase, base_path: Path = Path("."))
     with file_path.open('r') as f:
         lines = f.readlines()
         for line in lines:
-            path = Path(line.strip())
-            if not path.is_absolute():
-                path = resolved_base_path / path
-            
-            if path.exists() and path.is_file():
-                paths.append(path)
+            parse_space_separated_paths_escape(line, paths, resolved_base_path)
 
+    print(f'Changed files: {paths}')
     return paths
 
 def get_changed_files_from_module_helper(diagnostic: DiagnosticBase, module: Module, changed_source_files: List[Path], changed_files: MutableSet[DependencyFiles]) -> None:
